5ee15d549f8747e643bca28a3d7762c1b9df0f5f
Jfeng <kzjoy@163.com>
2020-12-31 10:28:36 +0800

    update

--- AppSearch/search.py
+++ AppSearch/search.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 import re
 import sys
+from copy import copy
 
 import threading
 
@@ -15,6 +16,8 @@ _headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
 }
 
+wf = Workflow3()
+
 
 class SearchItme:
     def __init__(self, uid, name, url, no_content, reg=''):
@@ -27,6 +30,49 @@ class SearchItme:
         self.icon = 'images/{0}.png'.format(name)
         self.post = False
         self.param = {}
+        self.timeout = 10
+
+    @property
+    def headers(self):
+        return _headers
+
+    def search(self, keyword):
+        self.url = self.url.format(keyword)
+        self.do_search()
+        url = self.url
+        content = self.content
+        arg = subtitle = url
+        if self.reg:
+            ret = re.search(re.compile(self.reg), content)
+            no_result = ret and ret.group() == self.no_content
+        else:
+            no_result = self.no_content in content
+        if no_result:
+            wf.logger.info('--> not result fund:' + self.no_content + ', name:' + self.name)
+            subtitle = u'没有找到内容'
+            arg = None
+        wf.add_item(keyword, subtitle, arg, uid=self.uid, icon=self.icon, valid=True)
+
+    def do_search(self):
+        url = self.url
+        try:
+            resp = web.get(url, headers=self.headers, timeout=self.timeout)
+            wf.logger.debug('--> ' + url + ' charset=' + resp.encoding)
+            self.content = resp.text
+        except Exception as e:
+            wf.logger.error("--> http error:" + url + " msg:" + e.message, exc_info=e)
+
+
+class Macbl(SearchItme):
+    def __init__(self):
+        url = 'https://www.macbl.com/search/{0}'
+        SearchItme.__init__(self, '6', 'macbl', url, '搜索不到')
+
+    @property
+    def headers(self):
+        headers = copy(_headers)
+        headers.setdefault('Accept-Language', 'zh-CN,zh;q=0.9,en;q=0.8')
+        return headers
 
 
 xclient = SearchItme('1', 'xclient', 'https://xclient.info/search/s/{0}', '没有找到内容')
@@ -34,8 +80,10 @@ macwk = SearchItme('2', 'macwk', 'https://www.macwk.com/soft/all/s-{0}/p1', '未
 macdrop = SearchItme('3', 'macdrop', 'https://macdrop.net/?s={0}&asl_active=1&p_asid=1', 'No results were found for this query')
 appked = SearchItme('4', 'appked', 'https://www.macbed.com/?s={0}', '0 Search results', '\\d+ Search results')
 macapps = SearchItme('5', 'macapps', 'https://macapps.to/?s={0}', 'No Posts found')
-_search_list = [xclient, appked, macdrop, macwk]
-# _search_list = [macdrop]
+macbl = Macbl()
+
+_search_list = [xclient, appked, macdrop, macwk, macbl]
+# _search_list = [macbl]
 
 
 def main(wf):
@@ -52,7 +100,6 @@ def main(wf):
     # Get args from Workflow3, already in normalized Unicode.
     # This is also necessary for "magic" arguments to work.
     args = wf.args
-
     # Do stuff here ...
 
     # Add an item to Alfred feedback
@@ -61,7 +108,7 @@ def main(wf):
         query = ' '.join(args)
         threads = []
         for itme in _search_list:
-            t = threading.Thread(target=_search, args=(wf, itme, query))
+            t = threading.Thread(target=itme.search, args=[query])
             threads.append(t)
             t.start()
         for t in threads:
@@ -69,48 +116,10 @@ def main(wf):
     else:
         wf.add_item('输入关键字', '等待查询')
     wf.warn_empty('没有找到内容', icon='icon.png')
-
     # Send output to Alfred. You can only call this once.
     # Well, you *can* call it multiple times, but subsequent calls
     # are ignored (otherwise the JSON sent to Alfred would be invalid).
     wf.send_feedback()
-
-
-def _search(wf, item, query):
-    item = _get_web_data(wf, item, query)
-    uid = item.uid
-    url = item.url
-    content = item.content
-    arg = subtitle = url
-    if item.reg:
-        ret = re.search(re.compile(item.reg), content)
-        no_result = ret and ret.group() == item.no_content
-    else:
-        no_result = content.count(item.no_content) > 0
-    if no_result:
-        wf.logger.info('--> not result fund:' + item.no_content + ', name:' + item.name)
-        # wf.logger.debug('--> content:' + content)
-        subtitle = u'没有找到内容'
-        arg = None
-        # uid = '99'
-    wf.add_item(query, subtitle, arg, uid=uid, icon=item.icon, valid=True)
-
-
-def _get_web_data(wf, item, query):
-    url = item.url.format(query)
-    timeout = 8
-    try:
-        if item.post:
-            param = _param_to_dic(item.param.format(query))
-            resp = web.post(url, param, headers=_headers, timeout=timeout)
-        else:
-            resp = web.get(url, headers=_headers, timeout=timeout)
-            wf.logger.debug('--> ' + url + ' charset=' + resp.encoding)
-        item.content = resp.text
-        item.url = url
-    except Exception as e:
-        wf.logger.error("--> http error:" + url + " msg:" + e.message, exc_info=e)
-    return item
 
 
 def _param_to_dic(param):
@@ -123,9 +132,9 @@ def _param_to_dic(param):
 
 
 if __name__ == '__main__':
-    # Create a global `Workflow3` object
-    wf = Workflow3()
+    # wf = Workflow3()
     # 测试
+    sys.argv.append('alfred')
     wf.alfred_env['debug'] = 1
     # Call your entry function via `Workflow3.run()` to enable its
     # helper functions, like exception catching, ARGV normalization,
